f6981bb9b5008cb7700412dd1fe3dcfd30a81b5c
Marcelo Garcia Pezzolla <mgarcpez@everis.com>
2020-12-09 09:46:37 -0300

    Add project files.

---
+++ BecaDotNet/ContaBancaria.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BecaDotNet
+{
+    class ContaBancaria
+    {
+        public double Saldo { get; private set; }
+        public string NumeroConta { get; private set; }
+        public string NomeTitular { get; set; }
+
+
+
+        public ContaBancaria(string numeroConta, string nome, double saldo)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nome;            
+            Saldo = saldo;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + NumeroConta + ", Titular: " + NomeTitular + ", Saldo: $ " + Saldo.ToString("F2");
+        }
+
+
+        //entrada de dados
+        //numero da conta
+        //titular
+        //valor inicial s/n
+        //se sim => metodo construtor com valor inicial
+        //se não => metodo construtor com valor inicial
+
+        //metodos
+        //retorna dados da conta
+        //deposito
+        //saque
+
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor + 5;
+        }
+
+
+
+
+    }
+}
---
+++ BecaDotNet/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+
+namespace BecaDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeituraDados();
+            Console.ReadLine();
+
+        }
+
+        private static void LeituraDados()
+        {
+            Console.WriteLine("Digite o número da conta: ");
+            string conta = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome do titular: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Haverá depósito inicial (S/N)? ");
+            string escolhaDepositoInicial = Console.ReadLine();
+
+            double deposito = 0.0;
+
+            if (escolhaDepositoInicial.ToLower() == "s")
+            {
+                Console.WriteLine("Digite o valor do depósito inicial: ");
+                deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            CriarConta(conta, nome, deposito);
+        }
+
+
+        private static void CriarConta(string conta, string nome, double deposito)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria(conta, nome, deposito);
+            RetornaDadosIniciais(contaBancaria);
+        }
+
+        public static void RetornaDadosIniciais(ContaBancaria contaBancaria)
+        {
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(contaBancaria.ToString());
+            Deposito(contaBancaria);
+        }
+
+        public static void Deposito(ContaBancaria contaBancaria)
+        {
+            Console.WriteLine("Entre um valor para o depósito: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Deposito(valor);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(contaBancaria.ToString());
+            Saque(contaBancaria);
+        }
+
+        public static void Saque(ContaBancaria contaBancaria)
+        {
+            Console.WriteLine("Entre um valor para saque:");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Saque(valor);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(contaBancaria.ToString());
+        }
+
+    }
+}
